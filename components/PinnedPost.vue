5d2198fffdc2ca566640a5a77471d2182e4b0d3e
Narelsiak <michalekxddlol@gmail.com>
2025-05-16 00:24:16 +0200

    pinned post fix

--- components/PinnedPost.vue
+++ components/PinnedPost.vue
@@ -2,46 +2,84 @@
   <div
     class="p-4 rounded-lg shadow-sm bg-teal-950"
   >
-    <div class="flex">
-      <UserImgWithPopover :user="pinnedPost!.user" size="big" />
-      <div>
-        <h3 class="font-semibold text-lg text-green-600">{{ pinnedPost!.user.username }}</h3>
+    <div class="flex flex-col sm:flex-row gap-2">
 
-        <!-- Tryb wyświetlania i edycji -->
-        <div>
-          <p class="text-gray-200">{{ pinnedPost!.content }}</p>
+      <div class="flex flex-row sm:flex-col items-center sm:items-start gap-2">
+        <UserImgWithPopover :user="pinnedPost!.user" :size="isMobile ? 'small' : 'big'" />
+
+        <div class="flex items-center justify-between sm:mt-1 leading-tight w-full">
+          <div class="flex flex-col sm:items-start items-start">
+            <h3 v-if="isMobile" class="font-semibold text-xl sm:text-xl text-green-600">
+              {{ pinnedPost!.user.username }}
+            </h3>
+            <div class="text-sm sm:text-xs text-gray-500">{{ formatDateShort(pinnedPost!.createdAt) }}</div>
+          </div>
+          <div v-if="!pinnedPost!.isDeleted && isMobile" class="ml-auto flex flex-col items-end">
+            <UButton label="Wyróżniony" icon="i-lucide-pin" variant="ghost" class="pr-0"/>
+            <div class="flex gap-2">
+
+              <UModal v-if="userStore.isAdminOrModerator" v-model:open="showUnpinPostModal" text="Odepnij post" title="Odepnij post" close size="sm">
+                <UButton icon="i-lucide-pin-off" color="error" variant="ghost" />
+
+                <template #body>
+                  <div>
+                    Czy na pewno chcesz odpiąć post?
+                  </div>
+                </template>
+            
+                <template #footer>
+                  <div class="flex justify-end gap-2 ml-auto">
+                    <UButton label="Zamknij" color="error" @click="showUnpinPostModal = false"/>
+                    <UButton label="Odepnij post" @click="togglePostPin(pinnedPost!)"/>
+                  </div>
+                </template>
+              </UModal>
+
+              <UTooltip text="Odpowiedź na ten post">
+                <UButton icon="i-lucide-reply" size="md" color="info" variant="solid" />
+              </UTooltip>
+            </div>
+          </div>
         </div>
+      </div>
 
-        <small class="text-gray-500">{{ formatDate(pinnedPost!.createdAt) }}</small>
+      <!-- Treść posta -->
+      <div class="w-full flex flex-col min-h-full">
+        <h3 v-if="!isMobile" class="font-semibold text-lg text-green-600">{{ pinnedPost!.user.username }}</h3>
+
+        <p class="text-gray-200">
+          <span v-html="parsedContent" />
+        </p>
       </div>
 
       <div v-if="!pinnedPost!.isDeleted" class="ml-auto flex gap-2 flex-col justify-between items-end">
 
-        <div class="flex gap-1">
-          <UButton label="Wyrózniony post" icon="i-lucide-pin" variant="ghost"/>
+        <div v-if="!pinnedPost!.isDeleted && !isMobile" class="ml-auto flex flex-col items-end gap-2">
+          <UButton label="Wyróżniony" icon="i-lucide-pin" variant="ghost" class="pr-0"/>
+          <div class="flex gap-2">
 
-          <UModal v-if="userStore.isAdminOrModerator" v-model:open="showUnpinPostModal" text="Odepnij post" title="Odepnij post" close size="sm">
-            <UButton icon="i-lucide-pin-off" color="error" variant="ghost" />
+            <UModal v-if="userStore.isAdminOrModerator" v-model:open="showUnpinPostModal" text="Odepnij post" title="Odepnij post" close size="sm">
+              <UButton icon="i-lucide-pin-off" color="error" variant="ghost" />
 
-            <template #body>
-              <div>
-                Czy na pewno chcesz odpiąć post?
-              </div>
-            </template>
+              <template #body>
+                <div>
+                  Czy na pewno chcesz odpiąć post?
+                </div>
+              </template>
             
-            <template #footer>
-              <div class="flex justify-end gap-2 ml-auto">
-                <UButton label="Zamknij" color="error" @click="showUnpinPostModal = false"/>
-                <UButton label="Odepnij post" @click="togglePostPin(pinnedPost!)"/>
-              </div>
-            </template>
-          </UModal>
+              <template #footer>
+                <div class="flex justify-end gap-2 ml-auto">
+                  <UButton label="Zamknij" color="error" @click="showUnpinPostModal = false"/>
+                  <UButton label="Odepnij post" @click="togglePostPin(pinnedPost!)"/>
+                </div>
+              </template>
+            </UModal>
 
-          <UTooltip text="Odpowiedź na ten post">
-            <UButton icon="i-lucide-reply" size="md" color="info" variant="solid" />
-          </UTooltip>
+            <UTooltip text="Odpowiedź na ten post">
+              <UButton icon="i-lucide-reply" size="md" color="info" variant="solid" />
+            </UTooltip>
+          </div>
         </div>
-
         <reaction-segment v-if="pinnedPost" :post="pinnedPost" />
       </div>
     </div>
@@ -50,15 +88,53 @@
 </template>
 
 <script setup lang="ts">
-import { formatDate } from '~/helpers/date';
+import DOMPurify from 'dompurify';
+import { formatDateShort } from '~/helpers/date';
 import { useUserStore } from '~/stores/user';
 import { usePostsStore } from '~/stores/posts';
 
 useToast();
 
+const isMobile = ref(false);
+const isReady = ref(false);
+
+onMounted(() => {
+  isMobile.value = window.innerWidth < 640;
+  isReady.value = true;
+  window.addEventListener('resize', () => {
+    isMobile.value = window.innerWidth < 640;
+  });
+});
+
 const { pinnedPost, togglePostPin } = usePostsStore();
 const userStore = useUserStore();
 
 const showUnpinPostModal = ref(false);
+
+const parsedContent = ref('');
+
+onMounted(() => {
+  const processedContent = pinnedPost!.content.replace(/\[quote](.*?)\[\/quote]/gs, (_, quoteText) => {
+    return `
+      <blockquote class="border-l-4 border-emerald-500 bg-gray-50 dark:bg-gray-800 p-4 my-4">
+        <p class="text-gray-900 dark:text-gray-100">${quoteText.trim()}</p>
+      </blockquote>
+    `;
+  });
+
+  parsedContent.value = DOMPurify.sanitize(processedContent);
+});
+
+watch(() => pinnedPost!.content, (newContent) => {
+  const processedContent = newContent.replace(/\[quote](.*?)\[\/quote]/gs, (_, quoteText) => {
+    return `
+      <blockquote class="border-l-4 border-emerald-500 bg-gray-50 dark:bg-gray-800 p-4 my-4">
+        <p class="text-gray-900 dark:text-gray-100">${quoteText.trim()}</p>
+      </blockquote>
+    `;
+  });
+
+  parsedContent.value = DOMPurify.sanitize(processedContent);
+});
 
 </script>
